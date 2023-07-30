3d561a78f84026e4090d85edcf4a02f943ea43c5
Emiliano <edesejas@eagerworks.com>
2023-07-30 14:27:29 -0300

    chore: remove unused function

--- utils/misc.hpp
+++ utils/misc.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <unistd.h>
 #include "globalStructures.hpp"
 
 int getAvailableHandler(std::vector<HandlerState> handlerStates) {
@@ -29,27 +28,4 @@ bool convertToInt(const std::string& str, int *result) {
 
   // Check if the whole string was processed
   return pos == str.size();
-}
-
-bool MPI_Probe_timeout(int source, int tag, MPI_Comm comm, MPI_Status *status, double timeout) {
-    double elapsed_time = 0.0;
-    double start_time = MPI_Wtime();
-
-    while (elapsed_time < timeout) {
-      LOG_INFO << "Waiting for message";
-      int flag = 0;
-      MPI_Iprobe(source, tag, comm, &flag, status);
-      
-      if (flag) {
-        return true;
-      }
-
-      // 10ms
-      usleep(10000);
-
-      double current_time = MPI_Wtime();
-      elapsed_time = current_time - start_time;
-    }
-
-    return false;  // Timeout
 }
